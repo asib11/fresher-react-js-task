d395f7ea39a423522ba39f643527ae0861563ad0
asib <ahmedasib1@gmail.com>
2023-11-02 16:36:41 +0600

    grid layout done and data load from json file

--- src/components/GallaryImage.jsx
+++ src/components/GallaryImage.jsx
@@ -1,30 +1,91 @@
-import { useState } from "react";
-import image1 from '/images/image-1.webp'
-import image2 from '/images/image-2.webp'
-import image3 from '/images/image-3.webp'
-import image4 from '/images/image-4.webp'
-import image5 from '/images/image-5.webp'
-import image6 from '/images/image-6.webp'
-import image7 from '/images/image-7.webp'
-import image8 from '/images/image-8.webp'
-import image9 from '/images/image-9.webp'
-import image10 from '/images/image-10.jpeg'
-import image11 from '/images/image-11.jpeg'
+import { useEffect, useState } from "react";
+// import image1 from '/images/image-1.webp'
+// import image2 from '/images/image-2.webp'
+// import image3 from '/images/image-3.webp'
+// import image4 from '/images/image-4.webp'
+// import image5 from '/images/image-5.webp'
+// import image6 from '/images/image-6.webp'
+// import image7 from '/images/image-7.webp'
+// import image8 from '/images/image-8.webp'
+// import image9 from '/images/image-9.webp'
+// import image10 from '/images/image-10.jpeg'
+// import image11 from '/images/image-11.jpeg'
 import Images from "./Images";
 
+// const images = [
+//     {
+//         id:1,
+//         image:image1,
+//     },
+//     {
+//         id:2,
+//         image:image2,
+//     },
+//     {
+//         id:3,
+//         image:image3,
+//     },
+//     {
+//         id:4,
+//         image:image4,
+//     },
+//     {
+//         id:5,
+//         image:image5,
+//     },
+//     {
+//         id:6,
+//         image:image6,
+//     },
+//     {
+//         id:7,
+//         image:image7,
+//     },
+//     {
+//         id:8,
+//         image:image8,
+//     },
+//     {
+//         id:9,
+//         image:image9,
+//     },
+//     {
+//         id:10,
+//         image:image10,
+//     },
+//     {
+//         id:11,
+//         image:image11,
+//     },
+// ]
 
 const GallaryImage = () => {
-    //Here array could have been used but useState hook is used if need to add image in future
-    const [images, setImage] = useState([image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11])
+    
+    const [images, setImage] = useState([]);
+
+    //data load
+    useEffect(() => {
+        fetch('images.json')
+        .then(response => response.json())
+        .then(data => setImage(data))
+    },[]);
+
+    //click checkbox and select
+    const handleSelect = id =>{
+        console.log(id)
+    }
+    
+
     return (
-        <div className='grid grid-cols-5 grid-flow-row gap-4'>
+        <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-flow-row gap-4'>
         
             {
                 images.map((image, index) => <Images
-                    key={index}
-                    image={image}
+                    key={image.id}
+                    id= {image.id}
+                    image={image.image}
                     index = {index}
-                    
+                    handleSelect = {handleSelect}
                 ></Images>
                 )
             }
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types'
 
-const Images = ({index,image}) => {
-    console.log(index);
+const Images = ({index,id,image,handleSelect}) => {
+    
+    // console.log(index);
     return (
-        <div className={`${index === 0 ? 'col-span-2 row-span-2': 'col-span-1 '} `}>
-            {/* {
-                index === 0? <img className='col-span-2' src={image} alt="" />: <img className='' src={image} alt="" />
-            } */}
-            <img className='' src={image} alt="" />
-            
+        <div className={`${index === 0 ? 'col-span-2 row-span-2': 'col-span-1 row-span-1'} relative `}>
+            <input onChange={() => handleSelect(id)} type="checkbox" name="" id="" className='absolute m-3 h-4 w-4 '/>
+            <img  className=' rounded border border-black' src={image} alt="" />
         </div>
     );
 };
 
 Images.propTypes ={
     image : PropTypes.any,
-    index: PropTypes.any
+    id: PropTypes.any,
+    index: PropTypes.any,
+    handleSelect: PropTypes.any
 }
 export default Images;
